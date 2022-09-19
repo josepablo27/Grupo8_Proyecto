8990202cfcecd13c382708115614b179c522b9f6
josepablo27 <jduran60412@ufide.ac.cr>
2022-09-18 23:04:13 -0600

    Primer commit

---
+++ src/main/java/com/Grupo8_Proyecto/Grupo8ProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.Grupo8_Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo8ProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo8ProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Grupo8_Proyecto/Grupo8ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grupo8_Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo8ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
